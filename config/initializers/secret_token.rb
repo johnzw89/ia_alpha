47d02bca700554c2ca4b1ec7cefcc16135e703c7
John Wei <johnzw89@gmail.com>
2013-07-09 23:30:00 -0400

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IaAlpha::Application.config.secret_token = '2e47b995d375cc65121a42f62dd255742132b2201a64f1db937d95b55a42b15b485d7ff1560864753ddcd39c3d51cb8cc32458b76885869f0f64c83dc2825d31'

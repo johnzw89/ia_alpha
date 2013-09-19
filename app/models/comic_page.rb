6c8dec7885fcd31b0fd8cecadc5e216d772ab482
John Wei <johnzw89@gmail.com>
2013-09-18 23:46:18 -0400

    html cosmetics... built nav grid

--- app/models/comic_page.rb
+++ app/models/comic_page.rb
@@ -1,6 +1,6 @@
 class ComicPage < ActiveRecord::Base
   attr_accessible :page_image, :page_number
-  has_attached_file :page_image, styles: { medium: "1024x768>",  thumb: "300x300>" }
+  has_attached_file :page_image, styles: { medium: "1024x768>",  thumb: "320x200>", grid: "120x80>" }
 
   validates :page_image, presence: true
   validates :page_number, presence: true
